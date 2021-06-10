ea2b14241a80fa1214ca6305ea03ad5657d7d297
rensilver <ren.oliv87@gmail.com>
2021-06-09 22:36:56 -0300

    student created

---
+++ student/src/main/java/com/rensilver/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.rensilver.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}

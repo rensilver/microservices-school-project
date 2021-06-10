7f20987ee5ec2fcce533c2696ad923f19428d81a
rensilver <ren.oliv87@gmail.com>
2021-06-09 23:37:15 -0300

    grade created

---
+++ grade/src/test/java/com/rensilver/grade/GradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rensilver.grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

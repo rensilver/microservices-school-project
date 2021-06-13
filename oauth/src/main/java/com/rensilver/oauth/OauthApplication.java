701cc43c0a48b1ab1572284fdd78bac55c9ce131
rensilver <ren.oliv87@gmail.com>
2021-06-13 13:39:52 -0300

    oauth microservice userfeignclient

--- oauth/src/main/java/com/rensilver/oauth/OauthApplication.java
+++ oauth/src/main/java/com/rensilver/oauth/OauthApplication.java
@@ -3,7 +3,9 @@ package com.rensilver.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class OauthApplication {
@@ -11,5 +13,4 @@ public class OauthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
 	}
-
 }

4c2a67844fc335c46e400f39c3a05b90b9d1333c
rensilver <ren.oliv87@gmail.com>
2021-06-13 11:52:47 -0300

    user and role entities, manytomany association - user microservice

--- student/src/main/java/com/rensilver/student/entities/StudentProfile.java
+++ student/src/main/java/com/rensilver/student/entities/StudentProfile.java
@@ -3,7 +3,6 @@ package com.rensilver.student.entities;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
